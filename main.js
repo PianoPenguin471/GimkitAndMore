f12c4168cc6cbaa61db42b1d5b5376be1dcc0ecf
PianoPenguin471 <78510897+PianoPenguin471@users.noreply.github.com>
2022-01-26 13:51:11 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 if (document.domain.includes("myvue.cascadetech.org")) {
-  for (var i=0;i<document.getElementsByClassName("mark").length;i++) {
-    document.getElementsByClassName("mark")[i].innerText = "A"
+  grades = document.getElementsByClassName("mark")
+  for (var i=0;i<grades.length;i++) {
+    if (grades[i].innerText === "N/A") continue;
+    grades[i].innerText = "A"
     console.log("fixed grade for " + i)
   }
 }
