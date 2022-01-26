b2f252b8044828a94e758f1292d73e69799bf5cd
PianoPenguin471 <78510897+PianoPenguin471@users.noreply.github.com>
2022-01-26 09:50:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 if (document.domain.includes("myvue.cascadetech.org")) {
-  document.getElementsByClassName("mark").forEach(schoolClass => {schoolClass.innerText = "A"})
+  document.getElementsByClassName("mark").forEach((schoolClass, index) => {
+    schoolClass.innerText = "A";
+    console.log("fixed element" + index);
+  })
 }
-alert(document.domain)
+alert(document.domain);
