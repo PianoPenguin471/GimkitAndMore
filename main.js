23424f9256eb9a10e37ebc1b4fe6255edc634d74
PianoPenguin471 <78510897+PianoPenguin471@users.noreply.github.com>
2022-01-26 09:40:51 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-if (document.domain.includes("myvue.cascadetech.org") {
+if (document.domain.includes("myvue.cascadetech.org")) {
   document.getElementsByClassName("mark").forEach(schoolClass => {schoolClass.innerText = "A"})
 }
 alert(document.domain)
